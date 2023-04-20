f9528fcb9e4b537c45c3bc15cdade4ad7ea75079
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-04-20 20:48:10 +0530

    rc slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import { code } from "@/data/code";
 
+/*
+ *   color
+ *   speed
+ *   font
+ *   source
+ *   font_size
+ */
+
+import Draggable, { DraggableBounds } from "react-draggable";
+import Slider from "rc-slider";
+
 export default function Home() {
-  const [fileData, setFileData] = useState(code);
-  const [text, setText] = useState("");
-  const [length, setLength] = useState(0);
   const [speed, setSpeed] = useState(10);
+  const [color, setColor] = useState("");
+  const [font, setFont] = useState("");
+  const [fontSize, setFontSize] = useState("");
+  const [source, setSource] = useState(code);
+
+  const [text, setText] = useState("");
   const [cursor, setCursor] = useState(0);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const codeRef = useRef<HTMLDivElement>(null);
@@ -19,9 +33,9 @@ export default function Home() {
   }, []);
 
   const addMoreText = () => {
-    if (text.length < fileData.length) {
-      const end = cursor + speed < fileData.length ? cursor + speed : undefined;
-      setText((prevText) => prevText + fileData.substring(cursor, end));
+    if (text.length < source.length) {
+      const end = cursor + speed < source.length ? cursor + speed : undefined;
+      setText((prevText) => prevText + source.substring(cursor, end));
       setCursor((oldCursor) => oldCursor + speed);
     }
   };
@@ -42,17 +56,13 @@ export default function Home() {
       const reader = new FileReader();
       reader.readAsText(file);
       reader.addEventListener("load", () => {
-        setFileData(reader.result as string);
+        setSource(reader.result as string);
       });
     }
   };
 
   return (
     <main className="">
-      <div>
-        <input type="file" onChange={fileHandler} />
-      </div>
-
       <div className="code" ref={codeRef}>
         <textarea className="code-area" ref={textAreaRef} onChange={changeHandler}></textarea>
         <pre className="code-pre">
@@ -61,6 +71,64 @@ export default function Home() {
           <div className="code-bottom"> </div>
         </pre>
       </div>
+
+      <Draggable>
+        <div className="window">
+          <div className="window__header">
+            <h4 className="window__title"> Settings </h4>
+            <div className="window__close">{"x"}</div>
+          </div>
+          <div className="window__body">
+            <div className="window__optionlist">
+              {/* color */}
+              <div className="window__option">
+                <span> Color </span>
+                <span> .... </span>
+              </div>
+              {/* speed */}
+              <div className="window__option">
+                <span> speed </span>
+                <span> .... </span>
+              </div>
+              {/* font size */}
+              <div className="window__option">
+                <span> Font Size </span>
+                <FontSlider />
+              </div>
+              {/* font family */}
+              <div className="window__option">
+                <span> Font Family </span>
+                <select name="" id="">
+                  <option value=""> Font 1 </option>
+                  <option value=""> Font 1 </option>
+                  <option value=""> Font 1 </option>
+                  <option value=""> Font 1 </option>
+                  <option value=""> Font 1 </option>
+                </select>
+              </div>
+              {/* source */}
+              <div className="window__option">
+                <span> Source </span>
+                <div>
+                  <input type="file" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Draggable>
     </main>
   );
 }
+
+const FontSlider = () => {
+  return (
+    <div className="window__slider">
+      <Slider
+        min={8}
+        max={50}
+        step={1}
+      />
+    </div>
+  );
+};
